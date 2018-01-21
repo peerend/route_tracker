f3e0004f90dfcc578497a910de966637a6e2c360
dylan <peerend@gmail.com>
2018-01-21 12:46:10 -0800

    add basic bootstrap template and map div on home

--- app/assets/javascripts/map_home.js
+++ app/assets/javascripts/map_home.js
@@ -1,2 +1,55 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function() {
+
+    //get the users location, returns general city coords if nothing else
+    function createMap (){
+      var geoLocationOpts = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      }
+      if (navigator.geolocation){
+        return navigator.geolocation.getCurrentPosition(positionFound, locationError, geoLocationOpts)        
+      } else {
+        alert('browser not supported');
+      }
+  
+      function positionFound(position){
+        var crd = position.coords;
+        var myCoords = {
+          lat: crd.latitude,
+          lon: crd.longitude,
+          zoom: 17
+        }
+        drawMap(myCoords);
+      }
+  
+      function locationError(error){
+        console.log(error.message);
+        var myCoords = {
+          lon: -122.6765,
+          lat: 45.5231,
+          zoom: 15
+        }
+        drawMap(myCoords);    
+      }
+    }
+  
+    function drawMap(myCoords){
+      var map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.OSM()
+          })
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat([myCoords.lon, myCoords.lat]),
+          zoom: myCoords.zoom
+        })
+      }); 
+    }
+  
+    createMap();
+  
+    //use google maps API to search for a bicycle store
+  });
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'map_home/index'
 
+  root 'map_home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
