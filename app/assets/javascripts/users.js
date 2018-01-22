0e0b8ecca717e42cf1e192b2af0f0ddc7a7a025a
dylan <peerend@gmail.com>
2018-01-21 18:21:19 -0800

    autofill home location based on current lat and lon

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,52 +1,36 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
 $(document).ready(function() {
+    
+    var getUserLocation = function(){
+        var geoLocationOpts = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+          }
+          if (navigator.geolocation){
+            return navigator.geolocation.getCurrentPosition(positionFound, locationError, geoLocationOpts)        
+          } else {
+            alert('browser not supported');
+          }
+    
+          function positionFound(position){
+              // if the user has no position, check where they are now and use that for the home address
+              if (!$("#user_home_lat").val()){
+                $("#user_home_lat").val(position.coords.latitude);
+              }
+              if (!$("#user_home_lon").val()){
+                $("#user_home_lon").val(position.coords.longitude);
+              }
+          }
+    
+          function locationError(){
+              console.log(error.message);
+          }
+    }
+    getUserLocation();
 
-    //get the users location, returns general city coords if nothing else
-    function createMap (){
-      var geoLocationOpts = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-      }
-      if (navigator.geolocation){
-        return navigator.geolocation.getCurrentPosition(positionFound, locationError, geoLocationOpts)        
-      } else {
-        alert('browser not supported');
-      }
-  
-      function positionFound(position){
-        var crd = position.coords;
-        var myCoords = {
-          lat: crd.latitude,
-          lon: crd.longitude,
-          zoom: 17
-        }
-        drawMap(myCoords);
-      }
-  
-      function locationError(error){
-        console.log(error.message);
-        var myCoords = {
-          lon: -122.6765,
-          lat: 45.5231,
-          zoom: 15
-        }
-        drawMap(myCoords);    
-      }
+    var getDestinationLocation = function(){
+        // here's where we'd add the geocoder to auto fill the destination lat and lon
     }
-  
-    function drawMap(myCoords){
-      var map = new ol.Map({
-        target: 'map',
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.OSM()
-          })
-        ],
-        view: new ol.View({
-          center: ol.proj.fromLonLat([myCoords.lon, myCoords.lat]),
-          zoom: myCoords.zoom
-        })
-      }); 
-    }
-    createMap();
-});
+})
